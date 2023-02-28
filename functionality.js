2b143fbc774d92ae69aa97d34a312acf41ace34f
Pranav662011 <79525414+Pranav662011@users.noreply.github.com>
2023-02-27 21:49:34 -0800

    Add files via upload

---
+++ functionality.js
@@ -0,0 +1,25 @@
+var form = document.getElementById('contact-form');
+
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    var name=form.elements.name.value;
+    var email=form.elements.email.value;
+    var message=form.elements.message.value;
+
+var database = firebase.database();
+
+database.ref('contacts').push({
+    name:name,
+    email: email,
+    message:message
+    }
+);
+form.reset();
+alert('thank you for your message!, Your message has been sent');
+});
+
+
+
+    
+
